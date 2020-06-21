e7504212044432a182f51bba27b2bbc1a1a1fb85
Abhijeet Kushwaha <abhijeetrkushwaha28@gmail.com>
2020-06-21 10:52:31 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+  state = {
+    post: []
+  }
+  componentDidMount() {
+    axios.get(process.env.REACT_APP_API)
+      .then((res) => {
+        this.setState({
+          post: res.data.slice(0, 10),
+        });
+        console.log(this.state.post);
+        
+        
+      });
+  }
+  render() {
+    
+    console.log(process.env.REACT_APP_HI);
+    
+    return (
+      <div className="App">
+        <h1>Abhijeet{process.env.REACT_APP_HI}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
+// class Github {
+//   constructor() {
+//     this.client_id = "4344cc18f43-2b5632ea4";
+//     this.client_secret = "42bfa0ba6ca7e7e0c46239f65ccfa6eff41ef4dc";
+//     this.repos_count = 5; //we dont wanna see all repos of an individual
+//     this.repos_sort = "created: asc";
+//   }
+//   async getUser(user) {
+//     const profileResponse = await fetch(
+//       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+//     );
+//     const repoResponse = await fetch(
+//       `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+//     );
+//     const profile = await profileResponse.json();
+//     const repos = await repoResponse.json();
+
+//     return {
+//       profile,
+//       repos,
+//     };
+//   }
+// }
