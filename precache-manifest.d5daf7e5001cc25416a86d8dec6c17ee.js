b32f3249c2800eef67c58c2dd3ab3a661766dc36
Abhijeet Kushwaha <abhijeetrkushwaha28@gmail.com>
2020-06-21 11:10:39 +0530

    Updates

---
+++ precache-manifest.d5daf7e5001cc25416a86d8dec6c17ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b52ac672bac2046d35933ca2b831bc",
+    "url": "/demo-api-react/index.html"
+  },
+  {
+    "revision": "d59c0f09221c9ccf784b",
+    "url": "/demo-api-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a4fd8e328f0bc33b1237",
+    "url": "/demo-api-react/static/js/2.0d11c82e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-api-react/static/js/2.0d11c82e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d59c0f09221c9ccf784b",
+    "url": "/demo-api-react/static/js/main.87611188.chunk.js"
+  },
+  {
+    "revision": "041204af7c70eb899acf",
+    "url": "/demo-api-react/static/js/runtime-main.fca6a5a6.js"
+  }
+]);
